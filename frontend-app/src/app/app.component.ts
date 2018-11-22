d9352e7b686d0efe468ee15ff1320cef0d1179ba
MiguelFs86 <miguel.fuentes.sobrin@gmail.com>
2018-11-22 23:10:08 +0100

    <feature> Minor changes and fixes

--- frontend-app/src/app/app.component.ts
+++ frontend-app/src/app/app.component.ts
@@ -9,6 +9,6 @@ export class AppComponent {
     
   title = 'frontend-app';
 //   static BACKEND_URL = 'https://51.38.184.194:8080';
-static BACKEND_URL = 'https://backend.serantes.pro';
+    static BACKEND_URL = 'http://backend.serantes.pro';
 //   static BACKEND_URL = 'http://backend-serantes.ddns.net';
 }
